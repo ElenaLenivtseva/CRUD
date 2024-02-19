b9a0c1d23f31ca72b8ccbe39a4b0479fd664e893
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-19 23:08:27 +0700

    Теперь список страниц создается с помощью map по выдуманному  массиву

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Modal from './components/Modal';
 
 function App() {
   const [modalOpen, setModalOpen] =useState(false)
+  const rows = [
+    {page: "Page 1", description: 'this is the first page', status: 'live'},
+    {page: "Page 2", description: 'this is the second page', status: 'draft'},
+    {page: "Page 3", description: 'this is the third page', status: 'error'},
+  ]
   return (
     <div className="App">
-      <Table/>
+      <Table rows={rows}/>
       <button className='btn' onClick={()=>setModalOpen(true)}>Add</button>
       {modalOpen && <Modal closeModal={()=>setModalOpen(false)}/>}
       {/* проверка, что гит работает */}
